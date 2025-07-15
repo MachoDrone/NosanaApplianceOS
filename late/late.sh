cb3dc611a0b43659e8863b55e20e261239428b52
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 08:42:01 -0400

    Update late.sh

--- late/late.sh
+++ late/late.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# apt-get update -y
+apt-get update -y
 dpkg -l | grep openssh
 apt install openssh-server -y
 dpkg -l | grep openssh-server
 dpkg -l | grep openssh
 
-wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
+|
+    for i in {1..3}; do
+      if wget -O /tmp/subtest.sh https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh; then
+        bash /tmp/subtest.sh && break
+      else
+        echo "Attempt $i failed, retrying in 10 seconds..."
+        sleep 10
+      fi
+    done
+
+
+#wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
 
 # The follwing did not print, pause, nor did they show an error.
 #echo "$(date)"
