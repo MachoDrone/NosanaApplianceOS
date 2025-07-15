2a73b2fce62271f0288d616073622d04eb4ea551
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 07:47:05 -0400

    Update late.sh

--- late/late.sh
+++ late/late.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "$(date)"
-echo "Current date: $(date)"
-echo "Press Enter to continue..."
-read -r
+# apt-get update -y
+dpkg -l | grep openssh
+apt install openssh-server -y
+dpkg -l | grep openssh-server
+dpkg -l | grep openssh
+
 wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
+
+# The follwing did not print, pause, nor did they show an error.
+#echo "$(date)"
+#echo "Current date: $(date)"
+#echo "Press Enter to continue..."
+#read -r
