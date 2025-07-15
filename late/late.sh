67f220850ce1ae3827cc053f0be6d92e939bff51
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 07:15:06 -0400

    Update late.sh

--- late/late.sh
+++ late/late.sh
@@ -3,4 +3,4 @@ echo "$(date)"
 echo "Current date: $(date)"
 echo "Press Enter to continue..."
 read -r
-# wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/cursor/prepare-remaster2-py-for-proxy-testing-2a6c/late/late.sh | bash
+wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
