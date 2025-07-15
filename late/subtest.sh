f014799e01abf9c883c1d4e27c77942076e57dba
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 18:05:21 -0400

    Update subtest.sh

--- late/subtest.sh
+++ late/subtest.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Instructions:
-# lead all commands with
-# echo " " && echo " " && echo " "
-# to easily spot the commands and the responses in Subiquity late run command.
-echo "---------------------------------------------------------"
 echo " " && echo " " && echo "This is a test run of sub script."
 
 echo " " && echo " " && echo "dpkg -l | grep iputils-ping"
