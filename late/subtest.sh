47881b957a709053f241389eb010d5db9c35d247
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 10:21:58 -0400

    Update subtest.sh

--- late/subtest.sh
+++ late/subtest.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+echo; echo
+echo -e "\033[32mThis is a test run of sub script.\033[0m"
+
+echo; echo
 echo "$(date)"
 echo "Current date: $(date)"
 
+echo; echo
 dpkg -l | grep iputils-ping
+echo; echo
 apt install iputils-ping -y
+echo; echo
 dpkg -l | grep iputils-ping
+echo; echo
