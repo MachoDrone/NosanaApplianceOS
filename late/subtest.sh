f904b8d91d609937e7707792505034823ac1865c
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 16:24:38 -0400

    Update subtest.sh

--- late/subtest.sh
+++ late/subtest.sh
@@ -2,20 +2,17 @@
 # Instructions:
 # lead all commands with
 # echo " " && echo " " && echo " "
-# to easily spot the commands and the responses in Subuquity late run command.
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
-curtin in-target -- dpkg -l | grep openssh-server
-
-echo -----------------------------------------------------------
+# to easily spot the commands and the responses in Subiquity late run command.
+echo "---------------------------------------------------------"
 echo " " && echo " " && echo "This is a test run of sub script."
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep iputils-ping"
-curtin in-target -- dpkg -l | grep iputils-ping
+echo " " && echo " " && echo "dpkg -l | grep iputils-ping"
+dpkg -l | grep iputils-ping
 
-echo " " && echo " " && echo "curtin in-target -- apt install iputils-ping -y"
-curtin in-target -- apt install iputils-ping -y
+echo " " && echo " " && echo "apt install iputils-ping -y"
+apt install iputils-ping -y
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep iputils-ping"
-curtin in-target -- dpkg -l | grep iputils-ping
+echo " " && echo " " && echo "dpkg -l | grep iputils-ping"
+dpkg -l | grep iputils-ping
 
-echo " " && echo " " 
+echo " " && echo " "
