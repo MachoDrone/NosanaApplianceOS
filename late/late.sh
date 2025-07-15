d882a829f19581813385f37a2ae140166196a70b
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 15:33:26 -0400

    Update late.sh

--- late/late.sh
+++ late/late.sh
@@ -1,62 +1,25 @@
 #!/bin/bash
 # Instructions:
 # lead all commands with
-# echo; echo; echo ""
-# to spot the command and response in Subuquity late run command.
+# echo " " && echo " " && echo " "
+# to easily spot the commands and the responses in Subuquity late run command.
+echo "--------------------------------------------------------------------------
+echo " " && echo " " && echo "This is a test run of Subiquity late run command."
+echo " " && echo " " && echo "$(date)"
 
-printf "\n\n \n"
-printf "\n\ntestmessage\n"
-echo "This is a test run of Subiquity late run command."
-
-echo " " && echo " "
-echo; echo; echo "sleep 3"
-sleep 3
-
-echo; echo; echo "$(date)"
-echo "Current date: $(date)"
-
-echo " " && echo "curtin in-target -- apt update -y"
+echo " " && echo " " && echo "curtin in-target -- apt update -y"
 curtin in-target -- apt update -y
-echo " " && echo "curtin in-target -- apt install wget -y"
+
+echo " " && echo " " && echo "curtin in-target -- apt install wget -y"
 curtin in-target -- apt install wget -y
 
-echo; echo; echo "dpkg -l | grep openssh-server"
- dpkg -l | grep openssh-server
+echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
+curtin in-target -- dpkg -l | grep openssh-server
 
-echo; echo; echo "apt install openssh-server -y"
+echo " " && echo " " && echo "curtin in-target -- apt install openssh-server -y"
 curtin in-target -- apt install openssh-server -y
 
-echo; echo; echo "dpkg -l | grep openssh-server"
-dpkg -l | grep openssh-server
+echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
+curtin in-target -- dpkg -l | grep openssh-server
 
-
-echo " " && echo " " && echo "-----------Actual subtest.sh--------------"
-for i in {1..3}; do
-  # Download in installer, pipe to target bash
-  if wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | curtin in-target -- bash; then
-    echo "Success!"
-    break
-  else
-    echo "Attempt $i failed, retrying in 10 seconds..."
-    sleep 10
-  fi
-done
-
-for i in {1..3}; do
-  # Download in installer, pipe to target bash
-  if wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest2.bat | curtin in-target -- bash; then
-    echo "Success!"
-    break
-  else
-    echo "Attempt $i failed, retrying in 10 seconds..."
-    sleep 10
-  fi
-done
-
-#wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
-
-# The follwing did not print, pause, nor did they show an error.
-#echo "$(date)"
-#echo "Current date: $(date)"
-#echo "Press Enter to continue..."
-#read -r
+wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
