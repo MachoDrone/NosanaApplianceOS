e4d3b8066c3c3c37db04b1d6306127c5467552d3
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-15 18:05:44 -0400

    Update late.sh

--- late/late.sh
+++ late/late.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# Instructions:
-# lead all commands with
-# echo " " && echo " " && echo " "
-# to easily spot the commands and the responses in Subiquity late run command.
-
 echo "--------------------------------------------------------------------------"
 echo " " && echo " " && echo "This is a test run of Subiquity late run command."
 echo " " && echo " " && echo "$(date)"
 
-echo " " && echo " " && echo "curtin in-target -- apt update -y"
+echo " " && echo " " && echo "curtin in-target -- apt update"
 curtin in-target -- apt update
 
 echo " " && echo " " && echo "curtin in-target -- apt install wget -y"
@@ -23,5 +18,6 @@ curtin in-target -- apt install openssh-server -y
 echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
 curtin in-target -- dpkg -l | grep openssh-server
 
-echo " " && echo " " && echo "curtin in-target -- wget and execute subtest.sh"
-curtin in-target -- wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
+echo " " && echo " " && echo "Download script to installer, copy to target, execute"
+wget -O /tmp/subtest.sh https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh
+cat /tmp/subtest.sh | curtin in-target -- sh -c "cat > /tmp/subtest.sh && bash /tmp/subtest.sh"
