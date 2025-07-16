c19ab907b5e920ced545c73bf0890bc1f29f05b9
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-16 07:13:17 -0400

    Update late.sh

--- late/late.sh
+++ late/late.sh
@@ -3,26 +3,26 @@ echo "--------------------------------------------------------------------------
 echo " " && echo " " && echo "This is a test run of Subiquity late run command."
 echo " " && echo " " && echo "$(date)"
 
-echo " " && echo " " && echo "curtin in-target -- apt update"
-curtin in-target -- apt update
+#echo " " && echo " " && echo "curtin in-target -- apt update"
+#curtin in-target -- apt update
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep wget"
-curtin in-target -- dpkg -l | grep wget
+#echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep wget"
+#curtin in-target -- dpkg -l | grep wget
 
-echo " " && echo " " && echo "curtin in-target -- apt install wget -y"
-curtin in-target -- apt install wget -y
+#echo " " && echo " " && echo "curtin in-target -- apt install wget -y"
+#curtin in-target -- apt install wget -y
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep wget"
-curtin in-target -- dpkg -l | grep wget
+#echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep wget"
+#curtin in-target -- dpkg -l | grep wget
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
-curtin in-target -- dpkg -l | grep openssh-server
+#echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
+#curtin in-target -- dpkg -l | grep openssh-server
 
-echo " " && echo " " && echo "curtin in-target -- apt install openssh-server -y"
+#echo " " && echo " " && echo "curtin in-target -- apt install openssh-server -y"
 # curtin in-target -- apt install openssh-server -y
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
-curtin in-target -- dpkg -l | grep openssh-server
+#echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep openssh-server"
+#curtin in-target -- dpkg -l | grep openssh-server
 
 echo " " && echo " " && echo "curtin in-target -- wget and execute subtest.sh"
 wget -O - https://raw.githubusercontent.com/MachoDrone/NosanaApplianceOS/refs/heads/main/late/subtest.sh | bash
