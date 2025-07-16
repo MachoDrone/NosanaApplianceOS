cd5eaaf322e428d075aa7e13cbdf29623f0cc7e1
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-07-16 07:21:53 -0400

    Update subtest.sh

--- late/subtest.sh
+++ late/subtest.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+echo "---------------------------------------------------------"
 echo " " && echo " " && echo "This is a test run of sub script."
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep iputils-ping"
-curtin in-target -- dpkg -l | grep iputils-ping
+#echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep iputils-ping"
+#curtin in-target -- dpkg -l | grep iputils-ping
 
-echo " " && echo " " && echo "curtin in-target -- apt install iputils-ping -y"
-curtin in-target -- apt install iputils-ping -y
+#echo " " && echo " " && echo "curtin in-target -- apt install iputils-ping -y"
+#curtin in-target -- apt install iputils-ping -y
 
-echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep iputils-ping"
-curtin in-target -- dpkg -l | grep iputils-ping
+#echo " " && echo " " && echo "curtin in-target -- dpkg -l | grep iputils-ping"
+#curtin in-target -- dpkg -l | grep iputils-ping
 
+echo "*********************************************************"
 echo " " && echo " "
